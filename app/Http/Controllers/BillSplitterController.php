816b289bd3feea5fd2aa775dc77d4009b183f952
Taylor Meyer <tam008@g.harvard.edu>
2018-04-02 21:17:43 -0400

    Updates to p3

---
+++ app/Http/Controllers/BillSplitterController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\BillSplitterCreateRequest;
+
+class BillSplitterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BillSplitterCreateRequest $request)
+    {
+        $splitterVal = 0;
+        $input = $request->except("_token");
+        $subTotal = $input['tab'] + ($input['service-tip'] * $input['tab'] / 100);
+        $splitterVal = number_format($subTotal / $_REQUEST['split_way'],2);
+        if (isset($input['round-up']) and $input['round-up'] == 'yes') {
+            $splitterVal = ceil($splitterVal);
+        }
+
+        $request->session()->flash('splitterVal', $splitterVal);
+        return redirect('/')->withInput($input);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,77 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
 
-        <title>Laravel</title>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <title>The Magic Bill Splitter</title>
+    <meta name='description' content='The Magic Bill Splitter'>
+    <meta name='author' content='Taylor Meyer Site'>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+          rel='stylesheet' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
+          crossorigin='anonymous'>
+    <link rel='stylesheet' href='css/styles.css'>
+</head>
 
-            .full-height {
-                height: 100vh;
-            }
+<body>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<h2 class="text-center">The Magic Bill Splitter</h2>
+{!! Form::open(['url' => 'save-data', 'method' => 'POST', "name"=>'bill-splitter', "id"=>'bill-splitter', "class"=>"billform"] ) !!}
+    <div class="row form-group">
+        <div class="col-sm-5 text-right">
+            <label >Split how many ways? <span>* Required</span></label>
         </div>
-    </body>
+        <div class="col-sm-5">
+            {{ Form::input('text', 'split_way', null, ['class' => 'form-control', 'id' => 'split_way']) }}
+        </div>
+    </div>
+    <div class="row form-group">
+        <div class="col-sm-5 text-right">
+            <label >How much was the tab? <span>* Required</span></label>
+        </div>
+        <div class="col-sm-5">       
+            {{ Form::input('text', 'tab', null, ['class' => 'form-control', 'id' => 'tab']) }}
+        </div>
+    </div>
+    <div class="row form-group">
+        <div class="col-sm-5 text-right">
+            <label >How was the service?</label>
+        </div>
+        <div class="col-sm-5">
+            {{ Form::select('service-tip', ['18'=>'Good (18% tip)', '15'=>'OK (15% tip)', '10'=>'Poor (10% tip)'], null, ["id"=>'service-tip', "class"=>"form-control"]) }}
+        </div>
+    </div>
+    <div class="row form-group">
+        <div class="col-sm-5 text-right">
+            <label >Round up?</label>
+        </div>
+        <div class="col-sm-5">
+            {{ Form::checkbox('round-up', 'yes', null, ['id'=>'round-up']) }} Yes
+        </div>
+    </div>
+    <br>
+    <div class="clearfix text-center">
+        <input type='submit' name='calculate' class='btn btn-primary' value='calculate' id='calculate'>
+    </div>
+    <br>
+
+    @if ($errors->any())
+        <div class='alert alert-danger'>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{  $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @if(session()->has('splitterVal'))
+        <div class="alert alert-success text-center">
+            Everyone owes ${{session()->get('splitterVal')}}
+        </div>
+    @endif
+
+{!! Form::close() !!}
+</body>
 </html>
